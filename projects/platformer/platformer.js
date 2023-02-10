85bc2c31cf4cb92496e8a076d75dab2d50f71e2c
1313sheyshey <Bello44310@Students.apsb.org>
2023-02-10 14:55:31 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -29,16 +29,16 @@ $(function () {
     // Create platforms  
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-    createPlatform (1,150,100,10)
-    createPlatform(300, 280, 100, 20)
-    createPlatform (150,400,500,20)
-    createPlatform (550,400,100,20)
-    createPlatform (400,300,100,20)
+    createPlatform (15,150,100,10)
+    createPlatform(600,300,100,20)
+    createPlatform (250,250,100,20)
+    createPlatform (150,550,100,20)
+    createPlatform (400,450,100,20)
     // TODO 2
     // Create collectables
-    createCollectable("steve", 150, 300, 10, 1);
-    createCollectable("grace",200, 20, 0.8);
-    createCollectable("diamond", 250, 150, 30, 0.2);
+    createCollectable("steve", 450, 300);
+    createCollectable("grace",200, 20,);
+    createCollectable("diamond", 600, 230);
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
@@ -50,9 +50,8 @@ $(function () {
     // TODO 3
     // Create cannons
     createCannon("left", 250, 1000); // cannon on left wall, 600px down, shooting once per second
-    createCannon("right", 450, 1000); // cannon on left wall, 600px down, shooting once per second
-    createCannon("left", 450, 1000); // cannon on left wall, 600px down, shooting once per second
-    createCannon("right", 670, 1000); // cannon on left wall, 600px down, shooting once per second
+    createCannon("right", 550, 1000); // cannon on left wall, 600px down, shooting once per second
+    createCannon("left", 600, 1000); // cannon on left wall, 600px down, shooting once per second
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay)
